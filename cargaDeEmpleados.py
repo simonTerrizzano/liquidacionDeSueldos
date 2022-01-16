df5bb89bd91d200c92846cd8b7d59697a8e08a3e
simonTerrizzano <97785846+simonTerrizzano@users.noreply.github.com>
2022-01-16 08:58:34 -0300

    Actualizacion

--- cargaDeEmpleados.py
+++ cargaDeEmpleados.py
@@ -2,69 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 
-
-
-# -------Clases-----------------------
-sueldosCategorias={"1": 82000.0, "2": 78000.0, "3": 75000.0, "4": 72500.0, "5": 69000.0}
-
-class Persona():
-	"""docstring for Persona"""
-	def __init__(self, DNI, nombre, edad):
-		self.nombre = nombre
-		self.edad = edad
-		self.DNI = DNI
-
-class Empleado(Persona):
-	"""docstring for Empleado"""
-	def __init__(self, categoria, antiguedad, DNI, nombre, edad, presentismo):
-		super().__init__(DNI, nombre, edad)
-		self.categoria = categoria
-		self.antiguedad= antiguedad
-		self.presentismo = presentismo
-	def estadoEmpleado(self):
-		print("Nombre: {} \nEdad {} años\nCategoria: {} \nAntiguedad: {} años \n".format(self.nombre, self.edad, self.categoria,self.antiguedad))
-
-class Sueldo():
-	"""docstring for Sueldo"""
-	def __init__(self, categoria, antiguedad, presentismo):
-		self.categoria = categoria
-		self.antiguedad = antiguedad
-		self.presentismo = presentismo
-	def liquidarSueldo(self):
-
-		def descuentoSueldo(sueldo, porsentaje):
-			descuento = (sueldo/100) * porsentaje
-			return descuento
-
-		sueldoBase=sueldosCategorias[str(self.categoria)]
-		sueldoBruto=sueldoBase
-		cabeceraLabel=Label(liquidacionFrame,text="     			REMUNERACIONES	   DESCUENTOS")
-		cabeceraLabel.grid(row=1,column=1,pady=10,padx=10)
-		basicoLabel=Label(liquidacionFrame,text="+Basico:			"+ str(sueldoBase))
-		basicoLabel.grid(row=2,column=1,pady=10,padx=10)
-		if self.presentismo != 1:
-			sueldoBruto=sueldoBase+(sueldoBase/10)
-			presentismoLabel=Label(liquidacionFrame,text="+Presentismo: 		"+ str(sueldoBase/10))
-			presentismoLabel.grid(row=3,column=1,pady=10,padx=10)
-		if self.antiguedad > 0:
-			sueldoBruto = sueldoBase + (sueldoBase/100)*(self.antiguedad*2)
-			antiguedadLabel=Label(liquidacionFrame,text="+Antiguedad:		"+ str((sueldoBase/100)*(self.antiguedad*2)))
-			antiguedadLabel.grid(row=4,column=1,pady=10,padx=10)
-		sueldoNeto = sueldoBruto
-		jubilacion = descuentoSueldo(sueldoBruto,11)
-		sueldoNeto-= jubilacion
-		jubilacionLabel=Label(liquidacionFrame,text="- Jubilacion: 						"+ str(jubilacion))
-		jubilacionLabel.grid(row=5,column=1,pady=10,padx=10)
-		sueldoNeto-= descuentoSueldo(sueldoBruto,2.5)
-		obraSocialLabel=Label(liquidacionFrame,text="- Obra Social: 						"+ str(descuentoSueldo(sueldoBruto,2.5)))
-		obraSocialLabel.grid(row=6,column=1,pady=10,padx=10)
-		sueldoNeto-= descuentoSueldo(sueldoBruto,3)
-		sindicatoLabel=Label(liquidacionFrame,text="- Sindicato: 						"+ str(round(descuentoSueldo(sueldoBruto,3),2)))
-		sindicatoLabel.grid(row=7,column=1,pady=10,padx=10)
-		brutoLabel=Label(liquidacionFrame,text="Sueldo Bruto: 		"+ str(sueldoBruto))
-		brutoLabel.grid(row=8,column=1,pady=10,padx=10)
-		netoLabel=Label(liquidacionFrame,text="Sueldo Neto: 						"+ str(round(sueldoNeto,2)))
-		netoLabel.grid(row=9,column=1,pady=10,padx=10)
 # -------Funciones_SQL-----------------------
 
 global miConexion
@@ -94,7 +31,7 @@ def conectarBBDD():
 def crearRegistro():
 	miConexion=sqlite3.connect("EMPLEADOS")
 	miCursor=miConexion.cursor()
-	miCursor.execute("INSERT INTO EMPLEADOS VALUES(NULL, '" + DNI.get() + "','" + NOMBRE.get() + "','" + EDAD.get() + "','" + CATEGORIA.get() + "','" + ANTIGUEDAD.get()+ "','" + PRESENTISMO.get() + "')")
+	miCursor.execute("INSERT INTO EMPLEADOS VALUES(NULL, '" + DNI.get() + "','" + NOMBRE.get() + "','" + EDAD.get() + "','" + CATEGORIA.get() + "','" + ANTIGUEDAD.get()+ "', NULL)")
 	miConexion.commit()
 	messagebox.showinfo("CREATE_SQL","Se a insertado exitosamente su registro")
 
@@ -105,24 +42,13 @@ def leerRegistro():
 	try:
 		miCursor.execute("SELECT * FROM EMPLEADOS WHERE DNI=" + DNI.get())
 		ElUsuario=miCursor.fetchall()
-		global lista
-		lista=[]
 		for usuario in ElUsuario:
 			DNI.set(usuario[1])
-			lista.append(usuario[1])
 			NOMBRE.set(usuario[2])
-			lista.append(usuario[2])
 			EDAD.set(usuario[3])
-			lista.append(usuario[3])
 			CATEGORIA.set(usuario[4])
-			lista.append(usuario[4])
 			ANTIGUEDAD.set(usuario[5])
-			lista.append(int(usuario[5]))
 			PRESENTISMO.set(int(usuario[6]))
-			lista.append(usuario[6])
-		empleado=Empleado(lista[3],lista[4],lista[0],lista[1],lista[2],lista[5])
-		sueldo=Sueldo(empleado.categoria,empleado.antiguedad,empleado.presentismo)
-		sueldo.liquidarSueldo()
 		miConexion.commit()
 	except sqlite3.OperationalError:
 		messagebox.showwarning("Campo vacío","Por favor rellene el campo DNI")
@@ -132,7 +58,7 @@ def leerRegistro():
 def actualizarRegistro():
 	miConexion=sqlite3.connect("EMPLEADOS")
 	miCursor=miConexion.cursor()
-	miCursor.execute("UPDATE EMPLEADOS SET NOMBRE='" + NOMBRE.get() + "', EDAD='" + EDAD.get() + "', CATEGORIA='" + CATEGORIA.get() + "', ANTIGUEDAD='" + ANTIGUEDAD.get() + "', PRESENTISMO='" + PRESENTISMO.get() + "' WHERE DNI=" + DNI.get())
+	miCursor.execute("UPDATE EMPLEADOS SET NOMBRE='" + NOMBRE.get() + "', EDAD='" + EDAD.get() + "', CATEGORIA='" + CATEGORIA.get() + "', ANTIGUEDAD='" + ANTIGUEDAD.get() + "', PRESENTISMO=NULL'" + "' WHERE DNI=" + DNI.get())
 	miConexion.commit()
 	messagebox.showinfo("UPDATE_SQL","Se a actualizado exitosamente su registro")
 
@@ -145,18 +71,18 @@ def borrarRegistro():
 		miConexion.commit()
 	messagebox.showinfo("DELETE_SQL","Se a eliminado exitosamente su registro")
 
-# -----Interfaz-Grafica----------------------
+# -----Interfaz-Grafica--------------------------------------------
 
+
+# -----Root--------------------------------
 
 root=Tk()
-root.title("Liquidación de Sueldos")
+root.title("Registro de Empleados")
 
 # -----Frames--------------------------------
 
 contenedor=Frame(root)
 contenedor.pack()
-liquidacionFrame=Frame(contenedor)
-liquidacionFrame.grid(row=1,column=2)
 superiorFrame=Frame(contenedor)
 superiorFrame.grid(row=1,column=1,pady=10,padx=10)
 inferiorFrame=Frame(contenedor)
@@ -201,7 +127,7 @@ barraMenu.add_cascade(label="Borrar",menu=borrarMenu)
 barraMenu.add_cascade(label="Ayuda",menu=ayudaMenu)
 
 # ----------Labels---------------------------
-DNILabel=Label(superiorFrame,textvariable="DNI:")
+DNILabel=Label(superiorFrame,text="DNI:")
 DNILabel.grid(row=1,column=1,pady=10,padx=10)
 
 NOMBRELabel=Label(superiorFrame,text="NOMBRE:")
